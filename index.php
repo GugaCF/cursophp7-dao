476c922855694f41e3705035c66357faba0083d9
Gustavo Campos Freitas <gugacf@yahoo.com.br>
2019-04-09 13:00:15 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -8,10 +8,24 @@ $usuarios = $sql->select("select * from tb_usuarios");
 
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
+/*$root = new Usuario();
 
 $root->loadById(3);
 
-echo $root;
+echo $root;*/
+
+/*$lista = Usuario::getlist();
+
+echo json_encode($lista);*/
+
+/*$search = Usuario::Search("jo");
+
+echo json_encode($search);*/
+
+$usuario = new Usuario();
+
+$usuario->login("root", "54321");
+
+echo $usuario;
 
  ?>
